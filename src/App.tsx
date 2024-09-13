7a45b4a230db3c7e275b8d61cfec8826dd9b7220
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-09-12 22:58:21 -0700

    That's one small step for Inji, one giant leap toward Inji Prime.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <hgroup>
+        <h1>1NJ57C3.dev</h1>
+        <p>Coming soon™</p>
+      </hgroup>
+
+      <footer>
+        <p>
+          Copyright © 2024 1NJ57C3. All Rights Reserved.
+        </p>
+      </footer>
+    </>
+  )
+}
+
+export default App
