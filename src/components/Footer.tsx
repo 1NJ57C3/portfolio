55889a0230c5b4edec898ddee276b48298b4b6d5
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-09-13 01:52:49 -0700

    feat(footer): add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p>Copyright © 2024 1NJ57C3. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+
+function Header() {
+  return (
+    <header role="banner">
+      <h3>
+        <a href="/">1NJ57C3.dev</a>
+      </h3>
+      <NavBar />
+    </header>
+  );
+}
+
+export default Header;
