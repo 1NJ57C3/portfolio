efbfbcaff80420a884edd9a087735f862c094964
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-09-13 06:03:46 -0700

    style(navbar): add css classes and navItem

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,24 @@
 function NavBar() {
-  const navItems = ["About", "Contact"];
+  const navItems: string[] = ["About", "Projects", "Contact"];
+
+  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    console.log((e.target as HTMLButtonElement).id);
+  }
 
   const navButtons = navItems.map((navItem, i) => (
-    <li key={i}>
-      <button>{navItem}</button>
+    <li key={i} className="nav-item">
+      <button onClick={(e) => handleClick(e)} id={navItem}>
+        {navItem}
+      </button>
     </li>
   ));
 
-  return <ul className="NavBar">{navButtons}</ul>;
+  return (
+    <nav className="NavBar">
+      <ul>{navButtons}</ul>
+    </nav>
+  );
 }
 
 export default NavBar;
