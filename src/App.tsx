b498d8cc32c4a59b902026e14432ddd35ee07ddd
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-10-02 01:31:06 -0700

    feat(app): add about and projects components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 // import Header from "./components/Header";
 import Landing from "./components/Landing";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 import Footer from "./components/Footer";
 import "./App.css";
 
@@ -7,7 +9,19 @@ function App() {
   return (
     <div className="App">
       {/* <Header /> */}
-      <Landing />
+      { /*
+        * animate after...
+          ? page load (when landing animatinos are complete)
+          ? scrolling past landing
+        * animation
+          ? make slide out from top
+          ? fade in directionally
+      */ }
+      <main>
+        <Landing />
+        <About />
+        <Projects />
+      </main>
       <Footer />
     </div>
   );
