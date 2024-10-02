a70f818b98ae7ed58436984d0ce9aeb45107f1a2
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-10-01 23:27:25 -0700

    feat(about): add first draft of bio

---
+++ src/pages/About.tsx
@@ -0,0 +1,36 @@
+function About() {
+  return (
+    <article className="About">
+      <p>
+        I&lsquo;m a front-end-leaning full-stack developer with a solid
+        foundation in React, JavaScript, TypeScript, and semantic HTML. I enjoy
+        crafting intuitive user interfaces and have experience with backend
+        technologies like Ruby on Rails. My approach focuses on writing
+        flexible, scalable code that can adapt as projects grow, making me
+        versatile across the stack.
+      </p>
+      <p>
+        Recently, I&lsquo;ve been switching from JavaScript to TypeScript and
+        exploring backend development with Django. My current priority is
+        refining my portfolio, which includes projects like a React Native
+        weather app. I&lsquo;m also working to deepen my backend skills and
+        strengthen my understanding of databases like PostgreSQL and SQLite.
+      </p>
+      <p>
+        At the core of my development process is a passion for problem-solving.
+        I enjoy tackling complex challenges, constantly learning, and building
+        maintainable solutions.
+      </p>
+      <p>
+        When I&lsquo;m not coding, you&lsquo;ll probably find me gaming,
+        listening to music on Spotify (mostly EDM like melodic bass/dubstep and
+        vocal trance, if you&lsquo;re savvy), or playing guitar (rather poorly).
+        You might also find me at the archery range or in a swimming pool to
+        clear my mind, although there&lsquo;s nothing better than driving...
+        probably a little too fast.
+      </p>
+    </article>
+  );
+}
+
+export default About;
